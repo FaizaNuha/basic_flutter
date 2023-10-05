3660892a4a1c7b9c994bcbac4da80d318934d382
FaizaNuha <mubarokfaiza@gmail.com>
2023-10-05 21:28:54 +0600

    Drawer, end drawer, body, container,row,coloum,bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -23,94 +23,138 @@ class Myapp extends StatelessWidget{
 
 class Myhome extends StatelessWidget{
   const Myhome({super.key});
-
-  ShowBanner(message,context){
-    return ScaffoldMessenger.of(context).showMaterialBanner(
-        MaterialBanner(content: Text("na na na na "), actions:[
-      ElevatedButton(onPressed: (){}, child: Icon(Icons.access_time_rounded)) //due
-    ],
-        ));
-  }
-
   MySnackBar(message,context){
-    return ScaffoldMessenger.of(context).showSnackBar(    //Had to be  context in Scaffold i.e return should be start with Scaffold
-        SnackBar(content: Text(message),
-          duration: Duration(seconds: 5),
-        )
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message),
+        duration: Duration(seconds: 3) ,
+      )
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+     return Scaffold(
+        endDrawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(child: Text("it is end drawer")),
 
-        appBar: AppBar(
-          title: Text("Welcome "),
-          titleSpacing: 2,
-          centerTitle: true,
-          elevation: 0,   // the shadow of the appbar
-          leading: Icon(Icons.add),
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(48),
-            child: Row(
-              children: [
-                Text("what is this"),
-                SizedBox(width: 20,),
-                Icon(Icons.add_business),
-                SizedBox(width: 20,),
+              ListTile(
+                title: Text("if there's already then no icon added for drawer. "),
 
-                Icon(Icons.add_business_rounded),
-                SizedBox(width: 20,),
+              )
+            ],
 
-                Icon(Icons.access_alarms),
-                SizedBox(width: 20,),
-
-
-                Icon(Icons.accessible_forward_outlined),
-
-
-
-              ],
-            ),
           ),
-          backgroundColor: Colors.grey,
-
-          actions: [
-            IconButton(onPressed: ()
-            {MySnackBar("naaaaananananananan",context);},
-                icon:Icon(Icons.access_alarm))
-          ],
-
-        ) ,
-      floatingActionButton: FloatingActionButton(onPressed: () {MySnackBar("i am so qool", context); },
-        child: Icon(Icons.add_business),
-        backgroundColor: Colors.black,
-
-
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
-      persistentFooterButtons: [
-        ElevatedButton(onPressed: (){}, child: Text("thank u")),
-        ElevatedButton(onPressed: (){}, child: Text("noo")),
-        ElevatedButton(onPressed: (){}, child: Text("yess")),
-        ElevatedButton(onPressed: (){}, child: Icon(Icons.add_business))
-      ], // in  the bottom
-
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: (){ShowBanner("fora", context);}, child: Icon(Icons.add))
-          ],
         ),
-      ),
 
-      );
+       appBar: AppBar(
+         title: Text("Drawer, end drawer,"),
+       ),
+       bottomNavigationBar: BottomNavigationBar(
+         currentIndex: 0,
+         items: [
+         BottomNavigationBarItem(icon: Icon(Icons.access_time_rounded),label: "home"),
+         BottomNavigationBarItem(icon: Icon(Icons.accessibility_new_rounded),label: "sweet home"),
+         BottomNavigationBarItem(icon: Icon(Icons.add),label: "add"),
+
+       ],
+         onTap: (int index){
+            if(index==0){
+              MySnackBar("tik ase sob? ", context);
+
+           }
+            if(index==1){
+              MySnackBar("yess", context);
+            }
+
+            if(index==2){
+              MySnackBar("Alhamdullilah", context);
+            }
+
+         },
+         
+       ),
+       drawer: Drawer(
+         child: ListView(
+           children: [
+             DrawerHeader(
+               padding: EdgeInsets.all(0),
+                 child: UserAccountsDrawerHeader(
+               decoration: BoxDecoration(
+                 color: Colors.orange,
+               ),
+               accountEmail: Text("abc@gamil.com"),
+               accountName: Text("nuah"),
+                   currentAccountPicture: ClipOval(
+                       child: Image.network("https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
+                       width: 80,
+                       height: 80,
+                       fit: BoxFit.cover),
+
+                   ),
+
+             )
+             ),
+
+             ListTile(
+                 leading: Icon(Icons.home),
+               title: Text("contant"),
+               onTap: (){MySnackBar("its happing", context);}),
+             ListTile(
+                 leading: Icon(Icons.phone_in_talk),
+                 title: Text("profile"),
+                 onTap: (){MySnackBar("its happing", context);}
+
+     ),
+             ListTile(
+               leading: Icon(Icons.mail_lock_rounded),
+               title: Text("email"),
+                 onTap: (){MySnackBar("its happing", context);},
+     ),
+             ListTile(
+             leading: Icon(Icons.ads_click_sharp),
+               title: Text("phone"),
+    onTap: (){MySnackBar("its happing", context);},
+    ),
+             ListTile(
+               leading: Icon(Icons.volunteer_activism_rounded),
+               title: Text("yop"),
+                 onTap: (){MySnackBar("its happing", context);}),
+
+           ],
+
+
+         ),
+       ),
+
+       body: Container(
+         width: 200,
+         height: 200,
+         alignment: Alignment.center,
+
+         margin: EdgeInsets.all(10),
+         padding: EdgeInsets.all(10),
+         decoration: BoxDecoration(
+           color: Colors.pink,
+           border: Border.all(color: Colors.black),
+
+         ),
+         child: Row(
+           mainAxisAlignment: MainAxisAlignment.spaceAround,
+           children: [
+             Column(
+               children: [
+                 Text("it's okay not to be okay")
+               ],
+             )
+           ],
+
+         ),
+       ),
+
+
+     );
   }
 
-
-
-
-}
+ }
